bd251afac7cb2b7e6d418f207db6983408495db7
Vlad <Vlad Bakh>
2023-04-01 16:25:00 +0300

    Initial Commit

---
+++ SushiShop/SushiShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SushiShopApp.swift
+//  SushiShop
+//
+//  Created by user on 01.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SushiShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
